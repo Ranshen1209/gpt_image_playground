bb6ae49bb2b40189d343fc48563d679d08c132d6
Jones <153377475+Ranshen1209@users.noreply.github.com>
2026-05-29 19:31:36 +0800

    chore: release v0.7.3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sakrylle-image-playground-v0.7.2'
+const CACHE_NAME = 'sakrylle-image-playground-v0.7.3'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png']
 
 self.addEventListener('install', (event) => {
