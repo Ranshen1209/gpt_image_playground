e509014e6d36939d7ae5d7055f83b9fd8a862cd1
Jones <153377475+Ranshen1209@users.noreply.github.com>
2026-05-31 16:11:58 +0800

    fix: restore circular theme reveal

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sakrylle-image-playground-v0.8.20'
+const CACHE_NAME = 'sakrylle-image-playground-v0.8.21'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './favicon.png']
 
 self.addEventListener('install', (event) => {
